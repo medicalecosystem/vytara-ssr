3e4e8f7e592679a97571f814eacbff055d6982a5
Aaron Pereira <aaronpereira2003@gmail.com>
2026-01-21 12:16:01 +0530

    Care Circle V1.1

--- src/lib/server.ts
+++ src/lib/server.ts
@@ -1,23 +1,23 @@
-// import { createServerClient } from "@supabase/ssr";
-// import { cookies } from "next/headers";
+import { createServerClient } from "@supabase/ssr";
+import { cookies } from "next/headers";
 
-// export async function supabaseServer() {
-//   const cookieStore = await cookies();
+export async function supabaseServer() {
+  const cookieStore = await cookies();
 
-//   return createServerClient(
-//     process.env.NEXT_PUBLIC_SUPABASE_URL!,
-//     process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
-//     {
-//       cookies: {
-//         getAll() {
-//           return cookieStore.getAll();
-//         },
-//         setAll(cookiesToSet) {
-//           cookiesToSet.forEach(({ name, value, options }) => {
-//             cookieStore.set(name, value, options);
-//           });
-//         },
-//       },
-//     }
-//   );
-// }
+  return createServerClient(
+    process.env.NEXT_PUBLIC_SUPABASE_URL!,
+    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
+    {
+      cookies: {
+        getAll() {
+          return cookieStore.getAll();
+        },
+        setAll(cookiesToSet) {
+          cookiesToSet.forEach(({ name, value, options }) => {
+            cookieStore.set(name, value, options);
+          });
+        },
+      },
+    }
+  );
+}
